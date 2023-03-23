2c9eaaabbabc6f8d9d698bdc1c72f0ad5eb3f189
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-23 20:55:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-
-
-const input = document.getElementById('button');
-
-function addingEventListener() {
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
   alert('I was clicked!');
-}
-
-input.addEventListener('click', addingEventListener);
+});
